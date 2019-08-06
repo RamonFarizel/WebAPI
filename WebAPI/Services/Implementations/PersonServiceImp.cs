cc481d799da67a04ec7c4729c0ee30de90e2351d
Farizel <ramonfarizel@gmail.com>
2019-08-06 16:03:30 -0300

    Finalizando vinculo dos 4 verbos com o banco de dados

--- WebAPI/Services/Implementations/PersonServiceImp.cs
+++ WebAPI/Services/Implementations/PersonServiceImp.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using WebAPI.Models;
 using WebAPI.Models.Context;
 
@@ -32,30 +33,53 @@ namespace WebAPI.Services.Implementations
 
         public void Delete(int id)
         {
-            
+            var result = _context.Persons.SingleOrDefault(p => p.ID.Equals(ID));
+            try
+            {
+                if (result != null)
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }   
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public IList<Person> FindAll()
         {
-            return new List<Person>
-            {
-                new Person { ID = 1, FirstName = "Ramon", LastName = "Farizel", AddressPerson = "Pavuna", Gender = "Masculino" },
-                new Person { ID = 2, FirstName = "Ramon", LastName = "Farizel", AddressPerson = "Pavuna", Gender = "Masculino" },
-                new Person { ID = 3, FirstName = "Ramon", LastName = "Farizel", AddressPerson = "Pavuna", Gender = "Masculino" },
-                new Person { ID = 4, FirstName = "Ramon", LastName = "Farizel", AddressPerson = "Pavuna", Gender = "Masculino" },
-                new Person { ID = 5, FirstName = "Ramon", LastName = "Farizel", AddressPerson = "Pavuna", Gender = "Masculino" },
-                new Person { ID = 6, FirstName = "Ramon", LastName = "Farizel", AddressPerson = "Pavuna", Gender = "Masculino" }
-            };
+            return _context.Persons.ToList();
         }
 
         public Person FindByID(int id)
         {
-            return new Person { ID = 1, FirstName = "Ramon", LastName = "Farizel", AddressPerson = "Pavuna", Gender = "Masculino" };
+            return _context.Persons.SingleOrDefault(p => p.ID.Equals(id));
         }
 
         public Person Update(Person person)
         {
+            if (!Exist(person.ID))
+                return new Person();
+
+            var result = _context.Persons.SingleOrDefault(p => p.ID.Equals(person.ID));
+            try
+            {
+                _context.Entry(result).CurrentValues.SetValues(person);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
             return person;
+        }
+
+        private bool Exist(int? iD)
+        {
+            return _context.Persons.Any(p => p.ID.Equals(iD));
         }
     }
 }
