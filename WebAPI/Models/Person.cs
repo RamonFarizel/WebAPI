b3ed7388908a1b9e5463d2b96d954cc16f1c298d
Farizel <ramonfarizel@gmail.com>
2019-08-06 12:45:46 -0300

    Gravando no banco local

--- WebAPI/Models/Person.cs
+++ WebAPI/Models/Person.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,8 +11,8 @@ namespace WebAPI.Models
         public long ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Address { get; set; }
-        public string gender { get; set; }
+        public string AddressPerson { get; set; }
+        public string Gender { get; set; }
 
     }
 }
--- WebAPI/Services/Implementations/PersonServiceImp.cs
+++ WebAPI/Services/Implementations/PersonServiceImp.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using WebAPI.Models;
+using WebAPI.Models.Context;
 
 namespace WebAPI.Services.Implementations
 {
     public class PersonServiceImp : IPersonService
     {
+        private SQLContext _context;
+
+        public PersonServiceImp(SQLContext context)
+        {
+            _context = context;
+        }
+
         public Person CreatePerson(Person person)
         {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+
             return person;
         }
 
@@ -20,18 +38,18 @@ namespace WebAPI.Services.Implementations
         {
             return new List<Person>
             {
-                new Person { ID = 1, FirstName = "Ramon", LastName = "Farizel", Address = "Pavuna", gender = "Masculino" },
-                new Person { ID = 2, FirstName = "Ramon", LastName = "Farizel", Address = "Pavuna", gender = "Masculino" },
-                new Person { ID = 3, FirstName = "Ramon", LastName = "Farizel", Address = "Pavuna", gender = "Masculino" },
-                new Person { ID = 4, FirstName = "Ramon", LastName = "Farizel", Address = "Pavuna", gender = "Masculino" },
-                new Person { ID = 5, FirstName = "Ramon", LastName = "Farizel", Address = "Pavuna", gender = "Masculino" },
-                new Person { ID = 6, FirstName = "Ramon", LastName = "Farizel", Address = "Pavuna", gender = "Masculino" }
+                new Person { ID = 1, FirstName = "Ramon", LastName = "Farizel", AddressPerson = "Pavuna", Gender = "Masculino" },
+                new Person { ID = 2, FirstName = "Ramon", LastName = "Farizel", AddressPerson = "Pavuna", Gender = "Masculino" },
+                new Person { ID = 3, FirstName = "Ramon", LastName = "Farizel", AddressPerson = "Pavuna", Gender = "Masculino" },
+                new Person { ID = 4, FirstName = "Ramon", LastName = "Farizel", AddressPerson = "Pavuna", Gender = "Masculino" },
+                new Person { ID = 5, FirstName = "Ramon", LastName = "Farizel", AddressPerson = "Pavuna", Gender = "Masculino" },
+                new Person { ID = 6, FirstName = "Ramon", LastName = "Farizel", AddressPerson = "Pavuna", Gender = "Masculino" }
             };
         }
 
         public Person FindByID(int id)
         {
-            return new Person { ID = 1, FirstName = "Ramon", LastName = "Farizel", Address = "Pavuna", gender = "Masculino" };
+            return new Person { ID = 1, FirstName = "Ramon", LastName = "Farizel", AddressPerson = "Pavuna", Gender = "Masculino" };
         }
 
         public Person Update(Person person)
