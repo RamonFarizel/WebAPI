d14218c5a96583c3301e398abf3586322b9a2c09
Farizel <ramonfarizel@gmail.com>
2019-08-06 11:11:01 -0300

    Estabelecendo coneão com o banco de dados

---
+++ WebAPI/Models/Context/SQLContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models.Context
+{
+    public class SQLContext : DbContext
+    {
+        public SQLContext()
+        {
+
+        }
+
+        public SQLContext(DbContextOptions<SQLContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Person> Persons {get;set; }
+    }
+}
