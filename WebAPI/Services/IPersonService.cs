cbb31c2336b252ce7c16a72929efe806d1eb3299
Farizel <ramonfarizel@gmail.com>
2019-08-06 09:38:18 -0300

    implementando classe Person

---
+++ WebAPI/Services/IPersonService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using WebAPI.Models;
+
+namespace WebAPI.Services
+{
+    interface IPersonService
+    {
+        Person CreatePerson(Person person);
+        Person FindByID(int id);
+        IList<Person> FindAll();
+        Person Update(Person person);
+        void Delete(int id);
+
+    }
+}
---
+++ WebAPI/Services/Implementations/PersonServiceImp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using WebAPI.Models;
+
+namespace WebAPI.Services.Implementations
+{
+    public class PersonServiceImp : IPersonService
+    {
+        public Person CreatePerson(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(int id)
+        {
+            
+        }
+
+        public IList<Person> FindAll()
+        {
+            return new List<Person>
+            {
+                new Person { ID = 1, FirstName = "Ramon", LastName = "Farizel", Address = "Pavuna", gender = "Masculino" },
+                new Person { ID = 1, FirstName = "Ramon", LastName = "Farizel", Address = "Pavuna", gender = "Masculino" },
+                new Person { ID = 1, FirstName = "Ramon", LastName = "Farizel", Address = "Pavuna", gender = "Masculino" }
+            };
+        }
+
+        public Person FindByID(int id)
+        {
+            return new Person { ID = 1, FirstName = "Ramon", LastName = "Farizel", Address = "Pavuna", gender = "Masculino" };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+    }
+}
